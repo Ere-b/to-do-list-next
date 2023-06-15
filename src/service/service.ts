b78616c47e43d6d0e4b4eb739e63bc1cb33166f1
Hanul <110656247+hanul-dev@users.noreply.github.com>
2023-06-15 20:23:34 +0900

    Feat connection to mongodb

---
+++ src/service/service.ts
@@ -0,0 +1,31 @@
+import { MongoClient } from "mongodb";
+
+const uri: string = process.env.MONGODB_KEY;
+// const dbName = "kyma-storage-1";
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (!process.env.MONGODB_URI) {
+  throw new Error("Please add your Mongo URI to .env.local");
+}
+
+if (process.env.NODE_ENV === "development") {
+  let globalWithMongoClientPromise = global as typeof globalThis & {
+    _mongoClientPromise: Promise<MongoClient>;
+  };
+  if (!globalWithMongoClientPromise._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    globalWithMongoClientPromise._mongoClientPromise = client.connect();
+  }
+  clientPromise = globalWithMongoClientPromise._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
