12e62c94e8556a4dc815bbcf0a5e8c9087903960
hanul <hanul.developer@gmail.com>
2023-06-16 17:47:22 +0900

    Feat: todo 수정 Function 추가

--- src/app/todolist/page.tsx
+++ src/app/todolist/page.tsx
@@ -1,19 +1,22 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
-import { Todo } from "../todo.model";
+import { Todo } from "../model/todo.model";
 
 const TodoListPage = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [isEdit, setIsEdit] = useState<Pick<Todo, "id"> | boolean>();
 
   const todoAddHandler = (text: string) => {
     setTodos((prevTodos) => [...prevTodos, { id: Math.random().toString(), text: text, done: false }]);
   };
 
-  // const todoEditHandler = (todoId: string) => {
-  //   setTodos((prevTodos) => (return;));
-  // };
+  const todoEditHandler = (todoId: string, text: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => (todo.id === todoId ? { ...todo, text: text } : todo));
+    });
+  };
 
   const todoDoneHandler = (todoId: string) => {
     setTodos((prevTodos) => {
@@ -26,6 +29,9 @@ const TodoListPage = () => {
       return prevTodos.filter((todo) => todo.id !== todoId);
     });
   };
+
+  const data = await fetch("/api/todos");
+  console.log(data);
 
   return (
     <div className="p-8">
